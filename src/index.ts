653595a6685ae662e9ddf5a5032a6568f3ea9c15
jwd222 <sji.2.sami@gmail.com>
2023-08-21 03:09:48 +0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import { expressMiddleware } from '@apollo/server/express4'
+import createApolloGraphqlServer from './graphql/index'
+
+async function init() {
+  const app = express()
+  const PORT = Number(process.env.PORT) || 8000
+
+  app.use(express.json())
+
+  app.get('/', (req, res) => {
+    res.json({ message: 'Server is up and running' })
+  })
+
+  app.use('/graphql', expressMiddleware(await createApolloGraphqlServer()))
+
+  app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`))
+}
+
+init()
